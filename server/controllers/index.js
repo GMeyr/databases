2bd0d64a7d563b7d6cbb9c2fe9bfa00cb498b60b
GMeyr <gabrielmeyr@gmail.com>
2015-12-19 14:55:49 -0500

    d middle place

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -5,7 +5,13 @@ var bluebird = require('bluebird');
 
 module.exports = {
   messages: {
-    get: function (req, res) {}, // a function which handles a get request for all messages
+    get: function (req, res) {
+      models.messages.get().then(function(messageData){
+        // res.header("Access-Control-Allow-Origin", "*");
+        // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        res.send(messageData);
+      });
+    }, // a function which handles a get request for all messages
     post: function (req, res) {} // a function which handles posting a message to the database
   },
 
@@ -15,4 +21,3 @@ module.exports = {
     post: function (req, res) {}
   }
 };
-
--- server/models/index.js
+++ server/models/index.js
@@ -1,18 +1,32 @@
 var db = require('../db');
-
-
-
+var Promise = require('bluebird');
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    get: function () {
+
+      return new Promise(function(resolve, reject) {
+        db.conn.query('select * from messages', function(error, results, fields){
+          if(error){
+            reject(error);
+          }
+          resolve(results);
+        });
+      });
+
+    }, // a function which produces all the messages
+    post: function () {
+
+    } // a function which can be used to insert a message into the database
   },
 
   users: {
     // Ditto as above.
-    get: function () {},
+    get: function () {
+      db.conn.query('select * from users', function(errors, results, fields){
+        return JSON.stringify(results);
+      })
+    },
     post: function () {}
   }
 };
-
